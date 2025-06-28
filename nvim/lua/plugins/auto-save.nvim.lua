db2d056b1b4109650041a553026a7fc9c8629beb
chensy <3290402927@qq.com>
2025-06-28 17:39:12 +0800

    add autosave support

---
+++ nvim/lua/plugins/auto-save.nvim.lua
@@ -0,0 +1,14 @@
+return {
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+        trigger_events = {
+            immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" },
+            defer_save = {},
+            cancel_deferred_save = {}
+        },
+        debounce_delay = 300
+    },
+}
