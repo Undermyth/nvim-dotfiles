3781efbb58a59762ab755ed110e329e25881cfd5
chensy <3290402927@qq.com>
2025-06-30 14:28:18 +0800

    add better scrolling and TODO support

---
+++ nvim/lua/plugins/neoscroll.nvim.lua
@@ -0,0 +1,10 @@
+return {
+    "karb94/neoscroll.nvim",
+    opts = {
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+        hide_cursor = false, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use local scrolloff instead of global scrolloff
+    },
+}
